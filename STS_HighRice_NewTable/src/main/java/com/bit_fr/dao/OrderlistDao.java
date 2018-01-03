6f57e2981a8e41f0ca3e28db5d68c6f4371094d3
jinssu <jinssu6489@naver.com>
2018-01-03 20:09:11 +0900

    MyBatis

--- STS_HighRice_NewTable/src/main/java/com/bit_fr/dao/OrderlistDao.java
+++ STS_HighRice_NewTable/src/main/java/com/bit_fr/dao/OrderlistDao.java
@@ -1,8 +1,59 @@
 package com.bit_fr.dao;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
+
+import com.bit_fr.db.OrderlistManager;
+import com.bit_fr.vo.OrderlistVo;
 
 @Repository
 public class OrderlistDao {
+
+	public int getCheckExist_orderlist(String member_id, int product_id) {
+		return OrderlistManager.getCheckExist_orderlist(member_id, product_id);
+	}
+
+	public int getCountNextOrderId_orderlist() {
+		return OrderlistManager.getCountNextOrderId_orderlist();
+	}
+
+	public int getCountToMyCondition_orderlist(String member_id, String condition) {
+		return OrderlistManager.getCountToMyCondition_orderlist(member_id, condition);
+	}
+
+	public List<OrderlistVo> getAll_orderlist() {
+		return OrderlistManager.getAll_orderlist();
+	}
+
+	public List<OrderlistVo> getMyOrderlistToCondition_orderlist(String member_id, String condition) {
+		return OrderlistManager.getMyOrderlistToCondition_orderlist(member_id, condition);
+	}
+
+	public int getCountMyOrderToCondition_orderlist(String member_id, String condition) {
+		return OrderlistManager.getCountMyOrderToCondition_orderlist(member_id, condition);
+	}
+
+	public List<OrderlistVo> getAllMyOrder_orderlist(String member_id) {
+		return OrderlistManager.getAllMyOrder_orderlist(member_id);
+	}
+
+	// Controller 에서 order_id 가 dao.getNUM_NextOrderID 으로 받아와짐.
+	public int insert_orderlist(OrderlistVo v) {
+		return OrderlistManager.insert_orderlist(v);
+	}
+
+	public int updateRentMonth_orderlist(String member_id, int product_id, int rent_month) {
+		return OrderlistManager.updateRentMonth_orderlist(member_id, product_id, rent_month);
+	}
+
+	// Controller 에서 order_id 가 dao.getNUM_NextOrderID 으로 받아와짐.
+	public int updateRentEnd_orderlist(int order_id) {
+		return OrderlistManager.updateRentEnd_orderlist(order_id);
+	}
+
+	public int delete_orderlist(String member_id, int product_id) {
+		return OrderlistManager.delete_orderlist(member_id, product_id);
+	}
 
 }
--- STS_HighRice_NewTable/src/main/java/com/bit_fr/db/OrderlistManager.java
+++ STS_HighRice_NewTable/src/main/java/com/bit_fr/db/OrderlistManager.java
@@ -1,10 +1,15 @@
 package com.bit_fr.db;
 
 import java.io.Reader;
+import java.util.HashMap;
+import java.util.List;
 
 import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import com.bit_fr.vo.OrderlistVo;
 
 public class OrderlistManager {
 	private static SqlSessionFactory factory;
@@ -19,4 +24,144 @@ public class OrderlistManager {
 			System.out.println(e);
 		}
 	}
+
+	public static int getCheckExist_orderlist(String member_id, int product_id) {
+		int count = -1;
+		SqlSession session = factory.openSession();
+
+		HashMap map = new HashMap();
+		map.put("member_id", member_id);
+		map.put("product_id", product_id);
+
+		count = session.selectOne("orderlist.getCheckExist_orderlist", map);
+		session.close();
+
+		return count;
+	}
+
+	public static int getCountNextOrderId_orderlist() {
+		int nextOrderID = -1;
+		SqlSession session = factory.openSession();
+
+		nextOrderID = session.selectOne("orderlist.getCountNextOrderId_orderlist");
+		session.close();
+
+		return nextOrderID;
+	}
+
+	public static int getCountToMyCondition_orderlist(String member_id, String condition) {
+		int count_MyProduct = -1;
+		SqlSession session = factory.openSession();
+
+		HashMap map = new HashMap();
+		map.put("member_id", member_id);
+		map.put("condition", condition);
+
+		count_MyProduct = session.selectOne("orderlist.getCountToMyCondition_orderlist", map);
+		session.close();
+
+		return count_MyProduct;
+	}
+
+	public static List<OrderlistVo> getAll_orderlist() {
+		SqlSession session = factory.openSession();
+
+		List<OrderlistVo> list = session.selectList("orderlist.getAll_orderlist");
+		session.close();
+
+		return list;
+	}
+
+	public static List<OrderlistVo> getMyOrderlistToCondition_orderlist(String member_id, String condition) {
+		SqlSession session = factory.openSession();
+
+		HashMap map = new HashMap();
+		map.put("member_id", member_id);
+		map.put("condition", condition);
+
+		List<OrderlistVo> list = session.selectList("orderlist.getMyOrderlistToCondition_orderlist", map);
+		session.close();
+
+		return list;
+	}
+	
+	public static int getCountMyOrderToCondition_orderlist(String member_id, String condition) {
+		int count = -1;
+		SqlSession session = factory.openSession();
+		
+		HashMap map = new HashMap();
+		map.put("member_id", member_id);
+		map.put("condition", condition);
+		
+		count = session.selectOne("orderlist.getCountMyOrderToCondition_orderlist", map);
+		session.close();
+		
+		return count;
+	}
+
+	public static List<OrderlistVo> getAllMyOrder_orderlist(String member_id) {
+		SqlSession session = factory.openSession();
+
+		List<OrderlistVo> list = session.selectList("orderlist.getAllMyOrder_orderlist", member_id);
+		session.close();
+
+		return list;
+	}
+
+	public static int insert_orderlist(OrderlistVo v) {
+		int re = -1;
+		SqlSession session = factory.openSession(true);
+
+		HashMap map = new HashMap();
+		map.put("next_NUM", v.getOrder_id());
+		map.put("member_id", v.getMember_id());
+		map.put("product_id", v.getProduct_id());
+		map.put("rent_start", v.getRent_start());
+		map.put("rent_month", v.getRent_month());
+
+		re = session.insert("orderlist.insert_orderlist", map);
+		session.close();
+
+		return re;
+	}
+
+	public static int updateRentMonth_orderlist(String member_id, int product_id, int rent_month) {
+		int re = -1;
+		SqlSession session = factory.openSession(true);
+
+		HashMap map = new HashMap();
+		map.put("member_id", member_id);
+		map.put("product_id", product_id);
+		map.put("rent_month", rent_month);
+
+		re = session.update("orderlist.updateRentMonth_orderlist", map);
+		session.close();
+
+		return re;
+	}
+
+	public static int updateRentEnd_orderlist(int order_id) {
+		int re = -1;
+		SqlSession session = factory.openSession(true);
+
+		re = session.update("orderlist.updateRentEnd_orderlist", order_id);
+		session.close();
+
+		return re;
+	}
+
+	public static int delete_orderlist(String member_id, int product_id) {
+		int re = -1;
+		SqlSession session = factory.openSession(true);
+
+		HashMap map = new HashMap();
+		map.put("member_id", member_id);
+		map.put("product_id", product_id);
+
+		re = session.delete("orderlist.delete_orderlist", map);
+		session.close();
+
+		return re;
+	}
+
 }
