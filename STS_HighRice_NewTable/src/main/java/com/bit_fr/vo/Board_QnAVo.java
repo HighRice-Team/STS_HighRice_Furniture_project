ba467ed5df802d1751d602c90db6f0e4263bfad6
JS <JS@JS-PC>
2018-01-03 20:16:50 +0900

    엉망진참임 ㅠㅠㅠㅠㅠ

--- STS_HighRice_NewTable/src/main/java/com/bit_fr/vo/Board_QnAVo.java
+++ STS_HighRice_NewTable/src/main/java/com/bit_fr/vo/Board_QnAVo.java
@@ -3,7 +3,7 @@ package com.bit_fr.vo;
 import java.util.Date;
 
 public class Board_QnAVo {
-	private String board_id;
+	private int board_id;
 	private String title;
 	private String member_id;
 	private int product_id;
@@ -12,74 +12,11 @@ public class Board_QnAVo {
 	private int b_ref;
 	private int b_level;
 
-	// Setter
-	public void setBoard_id(String board_id) {
-		this.board_id = board_id;
+	public Board_QnAVo() {
+		super();
 	}
 
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public void setMember_id(String member_id) {
-		this.member_id = member_id;
-	}
-
-	public void setProduct_id(int product_id) {
-		this.product_id = product_id;
-	}
-
-	public void setRegdate(Date regdate) {
-		this.regdate = regdate;
-	}
-
-	public void setContent(String content) {
-		this.content = content;
-	}
-
-	public void setB_ref(int b_ref) {
-		this.b_ref = b_ref;
-	}
-
-	public void setB_level(int b_level) {
-		this.b_level = b_level;
-	}
-
-	// Getter
-	public String getBoard_id() {
-		return board_id;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public String getMember_id() {
-		return member_id;
-	}
-
-	public int getProduct_id() {
-		return product_id;
-	}
-
-	public Date getRegdate() {
-		return regdate;
-	}
-
-	public String getContent() {
-		return content;
-	}
-
-	public int getB_ref() {
-		return b_ref;
-	}
-
-	public int getB_level() {
-		return b_level;
-	}
-
-	// Constructor
-	public Board_QnAVo(String board_id, String title, String member_id, int product_id, Date regdate, String content,
+	public Board_QnAVo(int board_id, String title, String member_id, int product_id, Date regdate, String content,
 			int b_ref, int b_level) {
 		super();
 		this.board_id = board_id;
@@ -92,9 +29,67 @@ public class Board_QnAVo {
 		this.b_level = b_level;
 	}
 
-	public Board_QnAVo() {
-		super();
-		// TODO Auto-generated constructor stub
+	public int getBoard_id() {
+		return board_id;
 	}
 
+	public void setBoard_id(int board_id) {
+		this.board_id = board_id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getMember_id() {
+		return member_id;
+	}
+
+	public void setMember_id(String member_id) {
+		this.member_id = member_id;
+	}
+
+	public int getProduct_id() {
+		return product_id;
+	}
+
+	public void setProduct_id(int product_id) {
+		this.product_id = product_id;
+	}
+
+	public Date getRegdate() {
+		return regdate;
+	}
+
+	public void setRegdate(Date regdate) {
+		this.regdate = regdate;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public int getB_ref() {
+		return b_ref;
+	}
+
+	public void setB_ref(int b_ref) {
+		this.b_ref = b_ref;
+	}
+
+	public int getB_level() {
+		return b_level;
+	}
+
+	public void setB_level(int b_level) {
+		this.b_level = b_level;
+	}
 }
