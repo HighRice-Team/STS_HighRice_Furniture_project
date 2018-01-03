3e43c5e3551f24a1f71c15f7953ea62a46e2f0e8
asete93 <34480319+asete93@users.noreply.github.com>
2018-01-03 20:52:46 +0900

    Rename MethodName vol.1

--- STS_HighRice_NewTable/src/main/java/com/bit_fr/controller/MemberController.java
+++ STS_HighRice_NewTable/src/main/java/com/bit_fr/controller/MemberController.java
@@ -2,17 +2,129 @@ package com.bit_fr.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.bit_fr.dao.MemberDao;
+import com.bit_fr.vo.MemberVo;
+//import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Controller
 public class MemberController {
 
 	@Autowired
-	private MemberDao dao;
+	private MemberDao member_dao;
 
 	public void setDao(MemberDao dao) {
-		this.dao = dao;
+		this.member_dao = dao;
+	}
+
+	//Select
+	@RequestMapping("/getAll_member.do")
+	public ModelAndView getAll_member() {
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("list", member_dao.getAll_member());
+
+		return mav;
+	}
+
+	@RequestMapping("/getOne_member.do")
+	public ModelAndView getOne_member(String member_id) {
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("v", member_dao.getOne_member(member_id));
+
+		return mav;
+	}
+
+	@RequestMapping("/getId_member.do")
+	public ModelAndView getId_member(MemberVo v) {
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("member_id", member_dao.getId_member(v));
+
+		return mav;
+	}
+
+	@RequestMapping("/getCount_member.do")
+	public ModelAndView getCount_member() {
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("countMember",member_dao.getCount_member());
+
+		return mav;
+	}
+
+	//Insert
+	
+	@RequestMapping(value = "/insert_member.do", method = RequestMethod.GET)
+	public void goToInsertMember() {
+	}
+	
+	@RequestMapping(value = "/insert_member.do", method = RequestMethod.POST)
+	public ModelAndView insert_member(MemberVo v) {
+		ModelAndView mav = new ModelAndView(); // 성공 시 요청 할 내용 입력해야 함 . ex) main 화면
+		int re = member_dao.insert_member(v);
+
+		if (re != 1) // insert가 실패 했을 경우.
+		{
+			mav.addObject("msg", "insert_member 등록 실패.");
+			mav.setViewName("fail");
+		}
+
+		return mav;
+	}
+	
+	//Update은 Ajax을 이용.
+	
+	
+	@RequestMapping(value="/updateResetPwd_member.do", produces = "text/plain;charset=utf-8")
+	@ResponseBody
+	public String updateResetPwd_member(String member_id)
+	{
+		String str = "";
+//		ObjectMapper om = new ObjectMapper();
+		int re = member_dao.updateResetPwd_member(member_id);
+		
+		try {
+//			str = om.writeValueAsString(re);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		return str;
+	}
+	
+	@RequestMapping(value="/updatePwd_member.do", produces = "text/plain;charset=utf-8")
+	@ResponseBody
+	public String updatePwd_member(MemberVo v)
+	{
+		String str = "";
+//		ObjectMapper om = new ObjectMapper();
+		
+		int re = member_dao.updatePwd_member(v);
+		
+		try {
+//			str = om.writeValueAsString(re);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		return str;
+	}
+	
+	@RequestMapping(value="/updateInfo_member.do", produces = "text/plain;charset=utf-8")
+	@ResponseBody
+	public String updateInfo_member(MemberVo v)
+	{
+		String str = "";
+//		ObjectMapper om = new ObjectMapper();
+		
+		int re = member_dao.updateInfo_member(v);
+		
+		try {
+//			str = om.writeValueAsString(re);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		return str;
 	}
 	
 	
--- STS_HighRice_NewTable/src/main/java/com/bit_fr/dao/MemberDao.java
+++ STS_HighRice_NewTable/src/main/java/com/bit_fr/dao/MemberDao.java
@@ -1,8 +1,43 @@
 package com.bit_fr.dao;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
+
+import com.bit_fr.db.MemberManager;
+import com.bit_fr.vo.MemberVo;
 
 @Repository
 public class MemberDao {
+	public List<MemberVo> getAll_member() {
+		return MemberManager.getAll_member();
+	}
 
+	public MemberVo getOne_member(String member_id) {
+		return MemberManager.getOne_member(member_id);
+	}
+
+	public String getId_member(MemberVo v) {
+		return MemberManager.getId_member(v);
+	}
+
+	public int getCount_member() {
+		return MemberManager.getCount_member();
+	}
+
+	public int insert_member(MemberVo v) {
+		return MemberManager.insert_member(v);
+	}
+
+	public int updateResetPwd_member(String member_id) {
+		return MemberManager.updateResetPwd_member(member_id);
+	}
+
+	public int updatePwd_member(MemberVo v) {
+		return MemberManager.updatePwd_member(v);
+	}
+
+	public int updateInfo_member(MemberVo v) {
+		return MemberManager.updateInfo_member(v);
+	}
 }
